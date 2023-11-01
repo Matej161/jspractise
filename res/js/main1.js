7269410fadd54f1669dce3252d4fde83828c5bfb
matej161 <matej.kulisek16@seznam.cz>
2023-11-01 10:17:11 +0100

    na hovno timer added

--- res/js/main1.js
+++ res/js/main1.js
@@ -1,14 +1,21 @@
 const point = document.getElementById("point");
 const startButton = document.getElementById("startButton");
+const timeInfo = document.getElementById("timeInfo");
+
 let gameInterval;
+let timeStart;
+
 startButton.onclick = () => {
   hideElement(startButton);
   startGameInterval(point);
+  setPointClick(point);
+  timeStart = performance.now();
 };
 
 point.onclick = () => {
   hideElement(startButton);
   movePoint(point, getRandomNumber(50, 1200), getRandomNumber(50, 600));
+  setSize(element, getRandomNumber(40, 80));
 };
 const movePoint = (element, x, y) => {
   element.style.top = `${y}px`;
@@ -21,11 +28,37 @@ const hideElement = (element) => {
 
 const startGameInterval = (element) => {
   clearInterval(gameInterval);
-
   gameInterval = setInterval(() => {
-    movePoint(element, getRandomNumber(50, 1200), getRandomNumber(50, 600));
+    setSize(element, getRandomNumber(40, 80));
+    movePoint(
+      element,
+      getRandomNumber(50, window.innerWidth - parseInt(element.style.width)),
+      getRandomNumber(50, window.innerHeight - parseInt(element.style.height))
+    );
   }, 550);
+  
 };
 
 const getRandomNumber = (minimum, maximum) =>
   Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+const setPointClick = (element) => {
+  element.onclick = () => {
+    element.innerText++;
+    setSize(element, getRandomNumber(40, 80));
+    movePoint(
+      element,
+      getRandomNumber(50, window.innerWidth / parseInt(element.style.width)),
+      getRandomNumber(50, window.innerHeight - parseInt(element.style.height))
+    );
+    let timeEnd = performance.now();
+    let result = timeEnd - timeStart;
+    timeInfo.innerText = `Time: ${result}ms`;
+    
+  };
+};
+
+const setSize = (element, size) => {
+  element.style.width = `${size}px`;
+  element.style.height = `${size}px`;
+};
